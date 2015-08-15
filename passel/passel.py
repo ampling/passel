2400064378d43f259402e6209f2a360ae27fd768
ampling <ampling@users.noreply.github.com>
2015-08-14 22:56:05 -0400

    reading stdin

--- passel/passel.py
+++ passel/passel.py
@@ -13,35 +13,34 @@ for people with a lot of digital credentials.
 
 from subprocess import call, Popen, PIPE
 from glob import glob
+import fileinput
 import argparse
 import gpgme
 
 
-# CONFIGURABLE {
 # Password Files:-$HOME/
 PASSWORD_STORE_DIR = '.password-store/test'
-# Let pass handle gpg
 use_Pass = 1
 #}
 
-# global variables #{
-home = os.path.expanduser("~")
-store = home + "/" + PASSWORD_STORE_DIR + "/"
+def  globs(): #{
+     home = os.path.expanduser("~")
+     store = home + "/" + PASSWORD_STORE_DIR + "/"
 #}
 
 #
 # BEGIN helper functions
 #
 
-#{ shell to Xclip
-def _copyXclip(text):
+# shell to Xclip
+def _copyXclip(text): #{
         p = Popen(['xclip', '-selection', 'c', '-l', '1', '-quiet'], stdin=PIPE,
                 close_fds=True)
         p.communicate(input=text.encode('utf-8'))
 #}
 
-#{ shell to pass
-def _pass(text):
+# shell to pass
+def _pass(text): #{
         p = Popen(['pass', ''], stdin=PIPE,
                 close_fds=True)
         p.communicate(input=text.encode('utf-8'))
@@ -53,7 +52,6 @@ def _pass(text):
 # BEGIN platform definable
 #
 
-#entry
 def entry(): #{
     global entry_file
     for file in entry_file:
@@ -64,7 +62,6 @@ def entry(): #{
         reclip()
 #}
 
-#reclip it
 def reclip(): #{
     global store
     global name
@@ -74,8 +71,7 @@ def reclip(): #{
 #}
 
 
-#{ Define Options
-def main():
+def main(): #{
     parser = argparse.ArgumentParser(description='Select some files', prog='passel')
     parser.add_argument("entry", help="Sends the entry to Xclip",
             type=str)
@@ -86,11 +82,18 @@ def main():
     global entry_file
     entry_file = list(str(args.entry))
     
+def read_From():
+    for line in fileinput.input():
+        print(line)
 
-    #entry_count = (len(entry_file))
-    #print(entry_file[entry_count - 1])
 #}
 
+def decypt_This():
+    for line in gnume
+
+
+
 if __name__ == '__main__':
-    main()
-    entry()
+    #main()
+    #entry()
+    read_From()
